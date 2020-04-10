95b71530f80181295b680723e1ff86d136de14ea
Damien Erambert <damien.erambert@me.com>
2020-04-09 23:27:36 -0700

    tweak itunes-refresh

--- bin/usr/itunes-refresh.sh
+++ bin/usr/itunes-refresh.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-# From https://github.com/beetbox/beets/issues/2967#issuecomment-465818544
-echo -e $1 | cut -c 3- | rev | cut -c 2- | rev | tr -d '\n' | xargs -0 osascript ~/.dotfiles/bin/usr/iTunesrefresh.scpt
+echo "Synchronizing music files..."
+rsync -rtvu --delete /Volumes/Damien\ OST/beets/ ~/Music/beets
+echo "Refreshing Music.app"
+osascript /Users/damien/.dotfiles/bin/usr/itunes-refresh.scpt /Users/damien/Music/beets/
