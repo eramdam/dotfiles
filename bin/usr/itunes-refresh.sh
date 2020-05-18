688a55c7675d253731161d15306a80d94f42ea0c
Damien Erambert <damien.erambert@me.com>
2020-05-17 18:06:56 -0700

    :wrench:

--- bin/usr/itunes-refresh.sh
+++ bin/usr/itunes-refresh.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo "Synchronizing music files..."
-rsync -rtvu --delete /Volumes/Damien\ OST/beets/ ~/Music/beets
 echo "Refreshing Music.app"
-osascript /Users/damien/.dotfiles/bin/usr/itunes-refresh.scpt /Users/damien/Music/beets/
+echo "argument: $1"
+osascript /Users/damien/.dotfiles/bin/usr/itunes-refresh.scpt "$1"
