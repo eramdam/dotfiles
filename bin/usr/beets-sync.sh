e6f97e40e9604023c9f83bee39e651098ef05e02
Damien Erambert <damien.erambert@me.com>
2020-06-06 23:21:59 -0700

    :fire:

--- bin/usr/beets-sync.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-echo "Synchronizing music files..."
-rsync -rtvu --delete /Volumes/Damien\ OST/beets/ ~/Music/beets
--- bin/usr/itunes-refresh.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-echo "Refreshing Music.app"
-echo "argument: $1"
-osascript /Users/damien/.dotfiles/bin/usr/itunes-refresh.scpt "$1"
